302222f8e89a35404da6fe0c8166b53a301252b1
EdisonAltamirano <43297927+EdisonAltamirano@users.noreply.github.com>
2023-05-06 20:49:03 -0600

    AP working

--- PLC_ESP/src/main.cpp
+++ PLC_ESP/src/main.cpp
@@ -11,33 +11,59 @@
 // #include <ModbusIP_ESP8266.h>
 
 // //////Modbus///p///
-// IPAddress remote(10, 22, 231, 238); // Address of Modbus Slave device
-// const int OUTPUTCOUNTOUT = 30;
-// int OUTPUTOFFSETOUT = 30;
-// uint16_t resoutputsout[OUTPUTCOUNTOUT];
-// uint16_t finalarray[OUTPUTCOUNTOUT];
-// ModbusIP mb; // ModbusIP object
-// //Funcion para enviar datos
-// bool cbinputs(Modbus::ResultCode event, uint16_t transactionId, void* data) { // Modbus Transaction callback
-//   if (event != Modbus::EX_SUCCESS)                  // If transaction got an error
-//     Serial.printf("Modbus result: %02X\n", event);  // Display Modbus error code
-//   if (event == Modbus::EX_TIMEOUT) {    // If Transaction timeout took place
-//     mb.disconnect(remote);              // Close connection to slave and
-//     mb.dropTransactions();              // Cancel all waiting transactions
-//   }
-//   if(event == Modbus::EX_SUCCESS)  {
-//     Serial.println("write outputs");
-//   }
-//   return true;
-// }
-
+IPAddress remote(10, 22, 231, 238); // Address of Modbus Slave device
+const int OUTPUTCOUNTOUT = 30;
+int OUTPUTOFFSETOUT = 30;
+uint16_t resoutputsout[OUTPUTCOUNTOUT];
+uint16_t finalarray[OUTPUTCOUNTOUT];
+ModbusIP mb; // ModbusIP object
+//Funcion para enviar datos
+bool cbinputs(Modbus::ResultCode event, uint16_t transactionId, void* data) { // Modbus Transaction callback
+  if (event != Modbus::EX_SUCCESS)                  // If transaction got an error
+    Serial.printf("Modbus result: %02X\n", event);  // Display Modbus error code
+  if (event == Modbus::EX_TIMEOUT) {    // If Transaction timeout took place
+    mb.disconnect(remote);              // Close connection to slave and
+    mb.dropTransactions();              // Cancel all waiting transactions
+  }
+  if(event == Modbus::EX_SUCCESS)  {
+    Serial.println("write outputs");
+  }
+  return true;
+}
+  bool res=false;
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
+    
+  //WiFiManager, Local intialization. Once its business is done, there is no need to keep it around
+  WiFiManager wm;
+  // put your setup code here, to run once:
+  // res = wm.autoConnect(); // auto generated AP name from chipid
+  // res = wm.autoConnect("AutoConnectAP"); // anonymous ap
+  res = wm.autoConnect("testesp32"); // password protected ap
+
+  if(!res) {
+      Serial.println("Failed to connect");
+      // ESP.restart();
+  } 
+  else {
+      //if you get here you have connected to the WiFi    
+      Serial.println("connected...yeey :)");
+  }
+
 }
+
 
 void loop() {
   // finalarray[0] = 3;
-  
+  // if(!res) {
+  //     Serial.println("Failed to connect");
+  //     // ESP.restart();
+  // } 
+  // else {
+  //     //if you get here you have connected to the WiFi    
+  //     Serial.println("connected...yeey :)");
+  // }
   // // // put your main code here, to run repeatedly:
   // if(!mb.writeHreg(remote, OUTPUTOFFSETOUT, finalarray, OUTPUTCOUNTOUT, cbinputs)){ // Try to Write array of COUNT of Coils to Modbus Slave
   //   Serial.print("#");
